fd0594aadfab2ced37844a1063aa186b57dd4319
SmartWalle <smartwalle@gmail.com>
2016-05-27 09:45:33 +0800

    update tet

--- video_test.go
+++ video_test.go
@@ -8,11 +8,11 @@ import (
 func TestGetVideoInfoParam(t *testing.T) {
 	var p = GetVideoInfoParam{}
 	p.VideoId = "27647891"
-	fmt.Println(RequestWithKey("e05b65109e8ecbeec5a61e276ac51592", "xxx", p))
+	fmt.Println(RequestWithKey("e05b65109e8ecbeec5a61e276ac51592", "exnwovajm7", p))
 }
 
 
 func TestGetVideoListParam(t *testing.T) {
 	var p = GetVideoListParam{}
-	fmt.Println(RequestWithKey("e05b65109e8ecbeec5a61e276ac51592", "xxx", p))
+	fmt.Println(RequestWithKey("e05b65109e8ecbeec5a61e276ac51592", "exnwovajm7", p))
 }
